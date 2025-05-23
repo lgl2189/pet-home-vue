083e4f7dc53f6cf905837aee45cc574717a2ddc6
lgl2189 <1214525891@qq.com>
2025-05-23 09:35:52 +0800

    添加获取推荐动物列表接口

--- src/apis/animal.js
+++ src/apis/animal.js
@@ -3,3 +3,11 @@ import request from '@/utils/request'
 export const uploadAnimalInfo = (formData) => {
   return request.post('/animal/upload/info', formData)
 }
+/**
+ * 获取推荐动物列表
+ * @param {number} num - 需要获取的动物数量
+ * @returns {Promise<AxiosResponse>} - 包含推荐动物列表的Promise
+ */
+export const getRecommendAnimalList = (num) => {
+  return request.get(`/animal/info/recommend?num=${num}`)
+}
