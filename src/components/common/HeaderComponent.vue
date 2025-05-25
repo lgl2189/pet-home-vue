01183dbc55103b1797cd60192145c77c958ce59d
lgl2189 <1214525891@qq.com>
2025-05-25 15:23:15 +0800

    修改刷新，修改菜单路由写法

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -17,7 +17,7 @@
       console.log('logout failed')
     }
     router.push('/')
-    location.reload(true)
+    location.reload()
   }
 
   const handleGetUserInfo = async () => {
@@ -51,51 +51,39 @@
       <router-link :to="{ name: 'HomeView' }">宠物之家</router-link>
     </div>
     <div class="navigator">
-      <el-menu class="navigator-menu" mode="horizontal" :ellipsis="true">
+      <el-menu class="navigator-menu" mode="horizontal" :ellipsis="true" :router="true" :default-active="$route.path">
         <el-sub-menu index="1">
           <template #title>动物</template>
-          <!-- 这里可以添加子菜单选项 -->
-          <el-menu-item index="1-1">
-            <router-link :to="{ name: 'AnimalUploadView' }">上传动物信息</router-link>
-          </el-menu-item>
-          <el-menu-item index="1-2">
-            <router-link :to="{ name: 'AnimalListView' }">动物列表</router-link>
-          </el-menu-item>
-          <el-menu-item index="1-3">动物详情</el-menu-item>
+          <el-menu-item index="/animal/upload" :route="{ name: 'AnimalUploadView' }">上传动物信息</el-menu-item>
+          <el-menu-item index="/animal/list" :route="{ name: 'AnimalListView' }">动物列表</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="2">
           <template #title>救助</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="2-1">救助流程</el-menu-item>
           <el-menu-item index="2-2">救助案例</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="3">
           <template #title>领养</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="3-1">可领养动物</el-menu-item>
           <el-menu-item index="3-2">领养申请</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="4">
           <template #title>志愿者</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="4-1">志愿者招募</el-menu-item>
           <el-menu-item index="4-2">志愿者活动</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="5">
           <template #title>捐赠</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="5-1">捐赠方式</el-menu-item>
           <el-menu-item index="5-2">捐赠记录</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="6">
           <template #title>展示</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="6-1">图片展示</el-menu-item>
           <el-menu-item index="6-2">视频展示</el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="7">
           <template #title>数据</template>
-          <!-- 这里可以添加子菜单选项 -->
           <el-menu-item index="7-1">统计数据</el-menu-item>
           <el-menu-item index="7-2">数据报表</el-menu-item>
         </el-sub-menu>
