124ebdf6834d43771804e2d01ffb11c572f06039
lgl2189 <1214525891@qq.com>
2025-06-23 23:44:42 +0800

    修正注册时未提供用户名给用户的问题

--- src/views/user/RegistView.vue
+++ src/views/user/RegistView.vue
@@ -15,6 +15,8 @@
     chinaId: '',
     realName: ''
   })
+  const dialogVisible = ref(false)
+  const dialogTipText = ref('')
   // 配置日期选择器选项，限制不能选择大于今天的日期
   const disabledDate = (time) => {
     return time.getTime() > Date.now()
@@ -72,7 +74,8 @@
       if (valid) {
         const res = await userRegist(form)
         if (res.status === '200') {
-          router.push('/user/login')
+          dialogTipText.value = res.data.user_id
+          dialogVisible.value = true
         } else {
           alert('提交失败')
         }
@@ -90,6 +93,11 @@
 </script>
 <template>
   <div class="container">
+    <el-dialog v-model="dialogVisible" width="500" title="Tips">
+      <div style="color: #00aeec"><router-link to="/user/login">注册成功，点击跳转到登陆界面</router-link></div>
+      <div>您的帐号为：{{ dialogTipText }}</div>
+      <el-button size="default" @click="dialogVisible = false">关闭</el-button>
+    </el-dialog>
     <div class="regist-form-wrap">
       <h1 class="regist-title">用户注册</h1>
       <el-form ref="formRef" :model="form" label-width="120px" class="regist-form" status-icon :rules="rules">
