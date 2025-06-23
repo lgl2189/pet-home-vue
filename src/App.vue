d67de97faf6d8e967973d0733728c921a0a7da24
lgl2189 <1214525891@qq.com>
2025-06-23 14:53:28 +0800

    添加控制Header和Footer开启和关闭的选项

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <script setup>
   import { useDark } from '@vueuse/core'
   import { ElConfigProvider } from 'element-plus'
+  import { computed } from 'vue'
+  import { useRoute } from 'vue-router'
   const zIndex = 3000
   const size = 'small'
+
+  const route = useRoute()
+  const isHideHeader = computed(() => {
+    return route.meta.isHideHeader === true
+  })
+  const isHideFooter = computed(() => {
+    return route.meta.isHideFooter === true
+  })
 </script>
 
 <template>
   <ElConfigProvider :size="size" :z-index="zIndex">
     <div id="app">
-      <div class="header-wrap">
+      <div class="header-wrap" v-if="!isHideHeader">
         <HeaderComponent />
       </div>
       <router-view></router-view>
-      <div class="footer-wrap">
+      <div class="footer-wrap" v-if="!isHideFooter">
         <FooterComponent />
       </div>
     </div>
